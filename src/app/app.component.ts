0d54a1838ccf9577861a8f5aef020b256e4358ce
Mixie Clerard <clerardm@gmail.com>
2020-01-13 21:53:38 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { MadLibs } from './madLibs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // madLibs: MadLibs;
+
   title = 'mad-libs';
 }
---
+++ src/app/madLibs.ts
@@ -0,0 +1,4 @@
+export class MadLibs {
+    noun: string;
+    adverb: string;
+  }
